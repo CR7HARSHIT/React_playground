eefc2c53be41b10e6b188fcc299307fbef2a42eb
CR7HARSHIT <hr2697020@gmail.com>
2024-09-20 22:38:21 +0530

    react apps created basics

---
+++ react-01/src/App.js
@@ -0,0 +1,13 @@
+import Jxx from './Practcie';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Hello</h1>
+    <Jxx/>
+    </>
+  );
+}
+
+export default App;
---
+++ react-01/src/Practcie.jsx
@@ -0,0 +1,7 @@
+function Jxx()
+{
+   return(
+         <h1>Harsh vardhan</h1>
+   );
+}
+ export default Jxx; 
