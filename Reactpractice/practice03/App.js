e811521ade3080015c6380cb8e01b234a5e6fcc9
CR7HARSHIT <hr2697020@gmail.com>
2024-09-22 15:58:41 +0530

    Fundamnetals of react

---
+++ Reactpractice/practice03/App.js
@@ -0,0 +1,8 @@
+const root=ReactDOM.createRoot(document.querySelector('#child1'));
+const str=React.createElement("p",{},"React for Child1")
+
+root.render(str)
+const root1=ReactDOM.createRoot(document.querySelector('#child2'));
+const str1=React.createElement("p",{},"React for Child2")
+
+root1.render(str1)
